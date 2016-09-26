1c3445efcf6cfab183a9a9bfc879720b5ab39d97
j22rebane <mariliis.kaldvee@gmail.com>
2016-09-26 21:22:13 +0300

    jade->html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('new_index.html', { title: 'ejs' });
+}); 
+//assuming app is express Object.
+/*
+router.get('/',function(req,res){
+       
+     res.sendFile(path.resolve(app.get('appPath') + '/new_index.html'));
+//     path.resolve(app.get('appPath') + '/index.html')
 
+});*/
 module.exports = router;
